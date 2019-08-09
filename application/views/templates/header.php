dcb98bcbe36db818154a1637c28d8bd6bea0370a
Sally Ross <46608728+SRAdda@users.noreply.github.com>
2019-08-09 15:05:33 -0700

    Uploads Master

---
+++ application/views/templates/header.php
@@ -0,0 +1,11 @@
+<?php
+// application/views/templates/header.php
+?>
+<html>
+
+        <head>
+                <title>Welcome to my chaotic world</title>
+        </head>
+        <body>
+
+                <h1><?=$title;?></h1>
